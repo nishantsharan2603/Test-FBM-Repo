822296d547716c0d228aa8416f199222cf19d9cf
abhijeet-sonawane-maersk <abhijeet.sonawane@maersk.com>
2026-01-29 02:48:18 +0530

    Create variables.tf
    AVD variables for FBM win 11 VMs

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "resource_group_name" {}
+variable "location" {}
+variable "hostpool_name" {}
+variable "vm_count" { type = number }
+variable "vm_name" {}
+variable "vm_size" {}
+variable "admin_username" {}
+variable "admin_password_keyvault" {}
+variable "key_vault_name" {}
+variable "key_vault_resource_group" {}
+variable "vnet_name" {}
+variable "subnet_name" {}

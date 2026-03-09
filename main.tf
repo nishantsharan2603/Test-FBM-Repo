bb0a85f5208dee1b69273868bc95c6406e303871
abhijeet-sonawane-maersk <abhijeet.sonawane@maersk.com>
2026-03-09 19:51:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -143,7 +143,7 @@ resource "azurerm_virtual_machine_extension" "intune_enroll" {
 
   settings = <<SETTINGS
 {
-  "commandToExecute": "powershell -ExecutionPolicy Bypass -Command \"Write-Host 'Waiting for Azure AD Join...'; do { \$status = dsregcmd /status | Out-String; Start-Sleep -Seconds 10 } until (\$status -match 'AzureAdJoined\\\\s*:\\\\s*YES'); Write-Host 'Azure AD Join detected. Starting Intune enrollment...'; Start-Process 'C:\\\\Windows\\\\System32\\\\dsregcmd.exe' -ArgumentList '/refreshprt' -Wait; Start-Process 'C:\\\\Windows\\\\System32\\\\DeviceEnroller.exe' -ArgumentList '/c /AutoEnrollMDM' -Wait; Write-Host 'Intune enrollment command executed.'\""
+  "commandToExecute": "powershell -ExecutionPolicy Bypass -Command \"Write-Host 'Waiting for Azure AD Join...'; do { $status = dsregcmd /status | Out-String; Start-Sleep -Seconds 10 } until ($status -match 'AzureAdJoined\\\\s*:\\\\s*YES'); Write-Host 'Azure AD Join detected. Starting Intune enrollment...'; Start-Process 'C:\\\\Windows\\\\System32\\\\dsregcmd.exe' -ArgumentList '/refreshprt' -Wait; Start-Process 'C:\\\\Windows\\\\System32\\\\DeviceEnroller.exe' -ArgumentList '/c /AutoEnrollMDM' -Wait; Write-Host 'Intune enrollment command executed.'\""
 }
 SETTINGS
 
